78905bec54051153367de02fcd1ae9d994839661
imerilo1100 <i.merilo.1100@gmail.com>
2015-02-15 14:54:03 +0200

    Külgriba.

--- leht/sidebar.php
+++ leht/sidebar.php
@@ -2,6 +2,13 @@
 <html>
     
 <div id = "kylgriba">
+Otsing:   <br>
+[Tulekul!]<p>
+<a href="index.php">PEALEHT</a> <br>
+SOOVITAJAD  <br>
+SOOVITA     <br>
+SAADA KUTSE <br>
+</p>
 </div>
 
 </html>
